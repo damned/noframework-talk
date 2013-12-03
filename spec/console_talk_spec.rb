2b2453d4569f6e0b7a13ebe4dcaf5f090fa531f8
danm <writetodan@yahoo.com>
2013-12-03 10:58:13 +0000

    @damned remove popen4

--- spec/console_talk_spec.rb
+++ spec/console_talk_spec.rb
@@ -1,14 +1,4 @@
-require 'popen4'
-
 describe 'talk' do
-  describe 'play' do
-    before :each do
-      status = POpen4::popen4( cmd_ ) do |stdout, stderr, stdin|
-          [stdout, stderr].each do |line|
-            puts line
-          end
-        end
-      puts
-    end
+  describe 'slideshow' do
   end
 end
